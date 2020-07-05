ed2e3d0268f5a4f301acb4b2963255f09c86acf5
robjhornby <robjhornby@gmail.com>
2020-07-05 20:22:44 +0100

    Added WavSource with some tests

---
+++ Experiment1/SoundSimulator/WavSource.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Reflection.Metadata;
+using NAudio.Wave;
+using Xunit;
+
+namespace SoundSimulator
+{
+    public class WavSource : ISoundSource
+    {
+        public Vector3 Position { get; set; }
+        private List<double> wavSamples = new List<double>();
+        private int rate;
+
+        public double EvaluateSouceAtTime(double evaluationTime)
+        {
+            var sampleN = (int)Math.Round(evaluationTime * (double)rate);
+            if(sampleN < 0 | sampleN > (wavSamples.Count - 1))
+            {
+                return 0.0;
+            }
+            return wavSamples[sampleN];
+        }
+
+        public WavSource(Vector3 pos, string wavPath)
+        {
+
+            Position = pos;
+            using (WaveFileReader reader = new WaveFileReader(wavPath))
+            {
+                Assert.Equal(16, reader.WaveFormat.BitsPerSample);
+                
+                rate = reader.WaveFormat.SampleRate;
+
+                byte[] bytesBuffer = new byte[reader.Length];
+                int read = reader.Read(bytesBuffer, 0, (int)reader.Length);
+
+                for (int sampleIndex = 0; sampleIndex < read / 2; sampleIndex++)
+                {
+                    var intSampleValue = BitConverter.ToInt16(bytesBuffer, sampleIndex * 2);
+                    wavSamples.Add(intSampleValue / 32768.0);
+                }
+            }
+        }
+    }
+
+
+}
--- Experiment1/SoundSimulatorTest/UnitTest1.cs
+++ Experiment1/SoundSimulatorTest/UnitTest1.cs
@@ -10,12 +10,12 @@ namespace SoundSimulatorTest
 {
     public class UnitTest1
     {
-        public static void ShowData(IEnumerable<double> result)
+        public static void ShowData(IEnumerable<double> result, string identifier="")
         {
             var subsample = result.Take(1000);
             var plt = new ScottPlot.Plot(800, 800);
             plt.PlotScatter(Enumerable.Range(0, subsample.Count()).Select(i => (double)i).ToArray(), subsample.ToArray());
-            plt.SaveFig(@$"C:\Users\nigel\Pictures\{DateTime.Now.ToFileTimeUtc()}.PNG");
+            plt.SaveFig(@$"C:\Users\nigel\Pictures\{DateTime.Now.ToFileTimeUtc()}{identifier}.PNG");
         }
 
         public static void ShowFFTData(IEnumerable<double> result, int samplerate)
@@ -33,6 +33,19 @@ namespace SoundSimulatorTest
             var plt = new ScottPlot.Plot(800, 800);
             plt.PlotScatter(freqs, fftPower);
             plt.SaveFig(@$"C:\Users\nigel\Pictures\{DateTime.Now.ToFileTimeUtc()}fft.PNG");
+        }
+        public static void WriteWav(IEnumerable<double> result, int rate = 44000, int bitDepth = 16, string identifier="")
+        {
+            byte[] buffer = result.SelectMany(r => BitConverter.GetBytes(Convert.ToInt16(Math.Max(Math.Min(1, r), -1) * 0.5 * 32768))).ToArray();
+
+            using (WaveFileWriter writer = new WaveFileWriter(@$"C:\Users\Rob\Pictures\{DateTime.Now.ToFileTimeUtc()}{identifier}.wav", new WaveFormat(rate, bitDepth, 1)))
+            {
+                //int bytesRead;
+                //while ((bytesRead = wavReader.Read(buffer, 0, buffer.Length)) > 0)
+                //{
+                writer.Write(buffer, 0, buffer.Length/*bytesRead*/);
+                //}
+            }
         }
         [Fact]
         public void TestPhaseCancel()
@@ -103,19 +116,56 @@ namespace SoundSimulatorTest
 
 
             //Assert.True(result.All(i => i < 0.0001));
+            string identifier = "Select1kHz_1mic_1source";
             ShowFFTData(scaledResult, SampleRate);
-            ShowData(scaledResult);
-            
-            byte[] buffer = scaledResult.SelectMany(r => BitConverter.GetBytes(Convert.ToInt16(r*32768))).ToArray();
+            ShowData(scaledResult, identifier: identifier);
+            WriteWav(scaledResult, identifier: identifier);
 
-            using (WaveFileWriter writer = new WaveFileWriter(@$"C:\Users\nigel\Pictures\{DateTime.Now.ToFileTimeUtc()}.wav", new WaveFormat(SampleRate, 16, 1)))
-            {
-                //int bytesRead;
-                //while ((bytesRead = wavReader.Read(buffer, 0, buffer.Length)) > 0)
-                //{
-                writer.Write(buffer, 0, buffer.Length/*bytesRead*/);
-                //}
-            }
         }
+
+        [Fact]
+        public void TestWavSource()
+        {
+            System.Diagnostics.Debug.WriteLine(Environment.CurrentDirectory);
+            Random randoCalrissian = new Random(123456789);
+            float magnitude = 10;
+            MicrophoneConfiguration microphoneConfiguration = new MicrophoneConfiguration()
+            {
+                Microphones = new List<IMicrophone>(Enumerable.Range(0, 45)
+                .Select(i =>
+                new Microphone()
+                {
+                    Position = new Vector3() { X = magnitude * (float)randoCalrissian.NextDouble(), Y = magnitude * (float)randoCalrissian.NextDouble(), Z = magnitude * (float)randoCalrissian.NextDouble() }
+                }))
+                //    new Microphone(){ Position = new Vector3(0,0,0)},
+                //    new Microphone(){ Position = new Vector3(1,0,0)},
+                //    new Microphone(){ Position = new Vector3(0,0,1)},
+                //    new Microphone(){ Position = new Vector3(0,1,0)},
+                //    new Microphone(){ Position = new Vector3(-1,0,0)}
+                //}
+            };
+
+            SourceCollection sourceCollection = new SourceCollection()
+            {
+                SoundSources = new List<ISoundSource>()
+                {
+                    new SteadySinSource(new Vector3(0,1,1),1000),
+                    new SteadySinSource(new Vector3(0,1,1),750),
+                    new SteadySinSource(new Vector3(1,1,0),500),
+                    new WavSource(new Vector3(1,1,1), $"source1.wav")
+                }
+            };
+            //var test = Constants.SOS;
+            Simulation sim = new Simulation(sourceCollection, microphoneConfiguration);
+            var result = sim.GetIntensityResult(0, 5, 1.0 / 44000, new Vector3(1, 1, 1));
+            //Assert.True(result.All(i => i < 0.0001));
+            string identifier = "WavTest_45mic_1source";
+            ShowData(result, identifier: identifier);
+            WriteWav(result, identifier: identifier);
+
+        }
+
+
+
     }
 }
